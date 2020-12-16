2c9e0c01d0a8f1448a838275c84832a4cba26157
Zach <zachary.lynch1337@gmail.com>
2020-12-16 15:50:18 -0800

    successfully made a call to api endpoint. Also created class to hold values

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,24 @@
+class Dog {
+    constructor(id, name, age, gender, breed){
+        this.id = id;
+        this.name = name;
+        this.age = age; 
+        this.gender = gender;
+        this.breed = breed;
+    }
+}
+let dogs = []
+
 fetch("http://127.0.0.1:3000/dogs")
     .then(res => {
         return res.json();
     })
     .then(json => {
-        console.log(json);
+        json.map((value,id) => {
+            dogs[id] = new Dog(value.id, value.name, value.age, value.gender, value.breed);
+        })
     });
+
+dogs.map(element => {
+    console.log(element.name);
+})
